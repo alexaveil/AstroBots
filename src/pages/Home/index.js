447c521615f3c2ef90be50d795a67a8b8e48a9bd
Michael P <m.polazhynets@hotmail.com>
2021-11-04 19:11:59 +0200

    Reworked section two and changed texts

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 import Grid from "@mui/material/Grid";
-import { styled } from '@mui/material/styles';
-import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { styled } from "@mui/material/styles";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
 import Container from "@mui/material/Container";
 
 import { Header, Footer } from "../../components";
@@ -14,12 +16,12 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: 30,
   borderRadius: 30,
   [`&.${linearProgressClasses.colorPrimary}`]: {
-    backgroundColor: 'transparent',
-    border: '0.5px solid rgba(255, 255, 255, 0.5)',
+    backgroundColor: "transparent",
+    border: "0.5px solid rgba(255, 255, 255, 0.5)",
   },
   [`& .${linearProgressClasses.bar}`]: {
     borderRadius: 30,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
 }));
 
@@ -37,49 +39,59 @@ const Home = () => {
           </div>
         </section>
         <section className={classes.sectionTwo}>
-          <div className={classes.sectionTwoLeft}>
-            <div className={classes.sectionTextWrapper}>
-              <div className={classes.mainTitle}>
-                <span>The Most</span>
-                <span className={classes.mainTitleRed}>advanced AI bots</span>
-              </div>
-              <div className={classes.mainText}>
-                <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                </p>
-                <p>
-                  Lorem Ipsum is simply dummy text of the printing and standard
-                  dummy text ever since the 1500s, when an unknown printer took
-                  a galley of type and scrambled it to make a type specimen
-                  book. It has survived not only five centuries
-                </p>
-                <p>
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className={classes.sectionTwoRight}>
-            <div className={classes.robotWrapper}>
-              <img src={RedRobot} className={classes.redRobotImage} />
-            </div>
-          </div>
+          <Container>
+            <Grid container>
+              <Grid item xs={12} sm={6}>
+                <div className={classes.mainTitle}>
+                  <span>The Most</span>
+                  <br />
+                  <span className={classes.mainTitleRed}>advanced AI bots</span>
+                </div>
+                <div className={classes.mainText}>
+                  <p>
+                    Powered by Open AI’s GPT-3, Astro Bots are able to
+                    <br /> give you fast and advanced answers to your
+                    <br /> questions while having fun and very deep
+                    <br /> conversations.
+                  </p>
+                  <p>
+                    Open AI was founded by Elon Musk and GPT-3 is
+                    <br /> the most advanced language generator, trained with
+                    <br />
+                    over 175 billion parameters.
+                  </p>
+                  <p>
+                    Each Astro’s goal is to be your own unique personal
+                    <br />
+                    assistant that will initially answer all of your questions
+                    <br />
+                    and chat with you but will later also help you learn,
+                    <br /> and assist you with your everyday tasks.
+                  </p>
+                </div>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <div className={classes.robotWrapper}>
+                  <img src={RedRobot} className={classes.redRobotImage} />
+                </div>
+              </Grid>
+            </Grid>
+          </Container>
         </section>
         <section className={classes.sectionThree}>
           <Container>
             <Grid container>
               <div className={classes.sectionThreeTitleWrapper}>
                 <div className={classes.sectionThreeTitle}>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
+                  Astro Bots is a collection of 8,888 original artworks of over
+                  more than 10,000,000 possible variations
                 </div>
                 <div className={classes.sectionThreeSubTitle}>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
+                  Every Astro Bot is an original artwork built with a huge
+                  collection of different unique variations of bodies,
+                  backgrounds, accessories, clothing and tattoos living
+                  eternally on the Ethereum blockchain. Every Astro is unique
+                  and special, however some are rarer than others.
                 </div>
               </div>
             </Grid>
@@ -106,42 +118,71 @@ const Home = () => {
               <div className={classes.sectionThreeTitleWrapper}>
                 <div className={classes.sectionThreeTitle}>Personality</div>
                 <div className={classes.sectionThreeSubTitle}>
-                  Each Bot has its own personality created by a set of
-                  characteristics. You'll get different answers and engage in
-                  different conversations based on on the Bot you own and its
+                  Each Astro has its own personality created by a set of
+                  characteristics. You’ll get different answers and engage in
+                  different conversations based on the Astro you own and its
                   personality.
                 </div>
               </div>
             </Grid>
             <Grid container>
-              <Grid item xs={12} sm={5} className={classes.redRobotImageWrapper}>
+              <Grid
+                item
+                xs={12}
+                sm={5}
+                className={classes.redRobotImageWrapper}
+              >
                 <img src={RedRobot} className={classes.redRobotImage} />
               </Grid>
               <Grid item xs={12} sm={7}>
                 <div className={classes.progressesWrapper}>
                   <div className={classes.progressItem}>
                     <div className={classes.progressItemText}>Empathy</div>
-                    <BorderLinearProgress className={classes.progressItemProgress} variant="determinate" value={50} />
+                    <BorderLinearProgress
+                      className={classes.progressItemProgress}
+                      variant="determinate"
+                      value={50}
+                    />
                   </div>
                   <div className={classes.progressItem}>
                     <div className={classes.progressItemText}>Kindness</div>
-                    <BorderLinearProgress className={classes.progressItemProgress} variant="determinate" value={30} />
+                    <BorderLinearProgress
+                      className={classes.progressItemProgress}
+                      variant="determinate"
+                      value={30}
+                    />
                   </div>
                   <div className={classes.progressItem}>
                     <div className={classes.progressItemText}>Intelligence</div>
-                    <BorderLinearProgress className={classes.progressItemProgress} variant="determinate" value={70} />
+                    <BorderLinearProgress
+                      className={classes.progressItemProgress}
+                      variant="determinate"
+                      value={70}
+                    />
                   </div>
                   <div className={classes.progressItem}>
                     <div className={classes.progressItemText}>Sarcasm</div>
-                    <BorderLinearProgress className={classes.progressItemProgress} variant="determinate" value={50} />
+                    <BorderLinearProgress
+                      className={classes.progressItemProgress}
+                      variant="determinate"
+                      value={50}
+                    />
                   </div>
                   <div className={classes.progressItem}>
                     <div className={classes.progressItemText}>Ellocuence</div>
-                    <BorderLinearProgress className={classes.progressItemProgress} variant="determinate" value={20} />
+                    <BorderLinearProgress
+                      className={classes.progressItemProgress}
+                      variant="determinate"
+                      value={20}
+                    />
                   </div>
                   <div className={classes.progressItem}>
                     <div className={classes.progressItemText}>Creative</div>
-                    <BorderLinearProgress className={classes.progressItemProgress} variant="determinate" value={80} />
+                    <BorderLinearProgress
+                      className={classes.progressItemProgress}
+                      variant="determinate"
+                      value={80}
+                    />
                   </div>
                 </div>
               </Grid>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -8,101 +8,64 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     display: "flex",
     justifyContent: "center",
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       height: "85vh",
-    }
+    },
   },
   robotWrapper: {
-    display: 'flex',
-    alignItems: 'flex-end',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "flex-end",
+    justifyContent: "center",
     height: "100%",
     maxWidth: "700px",
     width: "100%",
     position: "relative",
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       flexDirection: "column",
-      justifyContent: 'flex-end',
+      justifyContent: "flex-end",
     },
   },
   welcomeWrapper: {
     backgroundColor: "#ecebeb",
     fontSize: 18,
-    padding: '10px 25px',
+    padding: "10px 25px",
     position: "absolute",
     width: 150,
-    top: '35%',
+    top: "35%",
     right: 20,
     borderRadius: 10,
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       position: "relative",
       top: -20,
       right: 0,
       left: 0,
-      margin: '0 auto'
+      margin: "0 auto",
     },
   },
   robotImage: {
     maxWidth: 800,
     width: "100%",
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       maxWidth: 400,
-      margin: '0 auto'
+      margin: "0 auto",
     },
   },
   sectionTwo: {
-    height: "60vh",
-    display: 'flex',
-    [theme.breakpoints.down('sm')]: {
-      height: "auto",
-      flexDirection: "column"
-    },
-  },
-  sectionTwoLeft: {
-    width: "55%",
-    height: "100%",
-    display: 'flex',
-    justifyContent: 'flex-end',
-    [theme.breakpoints.down('lg')]: {
-      width: "100%",
-      padding: 20
-    },
-  },
-  sectionTextWrapper: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    paddingRight: 100,
-    width: "60%",
-    [theme.breakpoints.down('lg')]: {
-      width: "100%",
-      padding: 0
-    },
+    display: "flex",
+    padding: "50px 0",
+    backgroundColor: "#000000",
   },
   mainTitle: {
-    display: 'flex',
-    flexDirection: 'column',
     fontWeight: "bold",
     fontSize: 30,
+    color: "white",
     textTransform: "uppercase",
-    [theme.breakpoints.down('sm')]: {
-      alignItems: "center",
-    },
   },
   mainTitleRed: {
     color: "#d10000",
   },
-  sectionTwoRight: {
-    width: "45%",
-    height: "100%",
-    backgroundColor: "#d10000",
-    display: 'flex',
-    [theme.breakpoints.down('lg')]: {
-      width: "100%",
-    },
-    [theme.breakpoints.down('sm')]: {
-      justifyContent: "center",
-    },
+  mainText: {
+    color: "white",
   },
   redRobotImage: {
     maxWidth: 500,
@@ -110,53 +73,53 @@ const useStyles = makeStyles((theme) => ({
   },
   sectionThree: {
     backgroundColor: "#000000",
-    padding: '50px 0'
+    padding: "50px 0",
   },
   sectionThreeTitleWrapper: {
     color: "white",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    padding: "50px 0"
+    padding: "50px 0",
   },
   sectionThreeTitle: {
     textTransform: "uppercase",
     textAlign: "center",
     fontSize: 25,
     fontWeight: "bold",
-    maxWidth: '80%',
-    marginBottom: 20
+    maxWidth: "80%",
+    marginBottom: 20,
   },
   sectionThreeSubTitle: {
-    maxWidth: '80%',
-    fontSize: 14
+    maxWidth: "80%",
+    fontSize: 14,
   },
   itemImage: {
-    padding: 20
+    padding: 20,
   },
   itemImageBig: {
-    padding: 5
+    padding: 5,
   },
   progressesWrapper: {
     display: "flex",
     flexDirection: "column",
-    justifyContent: 'center',
-    padding: '50px'
+    justifyContent: "center",
+    padding: "50px",
   },
   progressItem: {
     display: "flex",
-    alignItems: 'center',
-    margin: '10px 0'
+    alignItems: "center",
+    margin: "10px 0",
   },
   progressItemText: {
     fontSize: 18,
-    fontWeight: 'bold',
-    color: 'white',
-    paddingRight: 20
+    fontWeight: "bold",
+    color: "white",
+    paddingRight: 20,
   },
   progressItemProgress: {
-    width: '100%',
-  }
+    width: "100%",
+  },
 }));
 
 export default useStyles;
